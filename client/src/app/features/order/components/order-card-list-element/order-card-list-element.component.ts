6263f66237d6014615cdf5a2d19f3bb82f3724c0
Paweł Reliński <pawelrelinski@gmail.com>
2021-12-31 12:21:25 +0100

    feat: Update UI orders list

--- client/src/app/features/order/components/order-card-list-element/order-card-list-element.component.ts
+++ client/src/app/features/order/components/order-card-list-element/order-card-list-element.component.ts
@@ -5,4 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './order-card-list-element.component.html',
   styleUrls: ['./order-card-list-element.component.scss'],
 })
-export class OrderCardListElementComponent {}
+export class OrderCardListElementComponent {
+  public readonly orders = [
+    {
+      orderNumber: 'WU414343',
+      datePlaced: 'Jul 6, 2020',
+      totalAmount: 240.99,
+      products: [
+        {
+          name: 'Product 1',
+          price: 70.0,
+          status: 'Delivered Jan 25, 2021',
+        },
+        {
+          name: 'Product 2',
+          price: 29.99,
+          status: 'Delivered Jan 25, 2021',
+        },
+        {
+          name: 'Product 3',
+          price: 125.0,
+          status: 'Delivered Jan 25, 2021',
+        },
+      ],
+    },
+    {
+      orderNumber: 'WU479452',
+      datePlaced: 'Aug 29, 2021',
+      totalAmount: 70.99,
+      products: [
+        {
+          name: 'Product 1',
+          price: 70.0,
+          status: 'Delivered Aug 31, 2021',
+        },
+      ],
+    },
+  ];
+}
